19c7c2687bd2bbc5e9ee20407a48167f90899846
David Hermo Blanco <davidhermo.tei@gmail.com>
2023-03-15 23:18:39 +0100

    solve database problems

---
+++ src/Controller/MailerController.php
@@ -0,0 +1,35 @@
+<?php
+
+// src/Controller/MailerController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Mime\Address;
+
+class MailerController extends AbstractController
+
+{
+    #[Route('/email')]
+    public function sendEmail(MailerInterface $mailer): Response
+    {
+        $email = (new Email())
+            ->from('hello@example.com')
+            ->to('you@example.com')
+            //->cc('cc@example.com')
+            //->bcc('bcc@example.com')
+            //->replyTo('fabien@example.com')
+            //->priority(Email::PRIORITY_HIGH)
+            ->subject('Time for Symfony Mailer!')
+            ->text('Sending emails is fun again!')
+            ->html('<p>See Twig integration for better HTML integration!</p>')
+            ->htmlTemplate('emails/signup.html.twig');
+
+        $mailer->sendEmail($email);
+
+        // ...
+    }
+}
